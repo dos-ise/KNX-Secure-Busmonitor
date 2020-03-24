04b8b88b230bd9f4dc969cb085532d4d8cf0f2d8
Dennis Ostermann <dennis.ostermann@ise.de>
2020-03-24 19:31:39 +0100

    added write ga UI

--- KNX Secure Busmonitor/KNX Secure Busmonitor/ViewModels/HomeViewModel.cs
+++ KNX Secure Busmonitor/KNX Secure Busmonitor/ViewModels/HomeViewModel.cs
@@ -12,7 +12,9 @@ namespace Busmonitor.ViewModels
 
   using Busmonitor.Model;
 
+  using Knx.Bus.Common;
   using Knx.Bus.Common.Configuration;
+  using Knx.Bus.Common.GroupValues;
   using Knx.Falcon.Sdk;
 
   using Plugin.Toast;
@@ -24,111 +26,122 @@ namespace Busmonitor.ViewModels
   public class HomeViewModel : INotifyPropertyChanged
   {
     private readonly Settings _settings;
+    private Bus _bus;
 
-    private string _connectButtonText;
-    private Color _connectButtonColor;
+    private string targetWriteAddress;
+    private string writeValue;
 
     public HomeViewModel(Settings settings)
     {
       _settings = settings;
-      _connectButtonText = "Connect";
-      _connectButtonColor = Color.GreenYellow;
+      _bus = new Bus(CreateParameter());
       Telegramms = new ObservableCollection<Telegramm>();
       ConnectCommand = new Command(OnConnect);
+      OpenWriteCommand = new Command(OnOpenWrite);
+      WriteCommand = new Command(OnWrite);
+      TargetWriteAddress = "1/1/1";
+      WriteValue = "true";
     }
 
     public ICommand ConnectCommand { get; }
+    public ICommand OpenWriteCommand { get; }
+    public ICommand WriteCommand { get; }
 
     public ObservableCollection<Telegramm> Telegramms { get; set; }
 
-    public string ConnectButtonText
+    public string ConnectButtonText => _bus.IsConnected ? "Connect" : "Disconnect";
+
+    public Color ConnectButtonColor => _bus.IsConnected ? Color.GreenYellow : Color.Red;
+
+    public string TargetWriteAddress
     {
-      get
-      {
-        return _connectButtonText;
-      }
+      get => targetWriteAddress;
       set
       {
-        _connectButtonText = value;
-        OnPropertyChanged(nameof(ConnectButtonText));
+        targetWriteAddress = value;
+        OnPropertyChanged(nameof(TargetWriteAddress));
       }
     }    
     
-    public Color ConnectButtonColor
+    public string WriteValue
     {
-      get
-      {
-        return _connectButtonColor;
-      }
+      get => writeValue;
       set
       {
-        _connectButtonColor = value;
-        OnPropertyChanged(nameof(ConnectButtonColor));
+        writeValue = value;
+        OnPropertyChanged(nameof(WriteValue));
       }
+    }
+
+    private void OnOpenWrite()
+    {
+
+    }
+    private void OnWrite()
+    {
+      _bus.WriteValue(new GroupAddress(TargetWriteAddress), GroupValue.Parse(WriteValue));
     }
 
     private void OnConnect()
     {
       ConnectorParameters connectorParameter = CreateParameter();
-      if (ConnectButtonText != "Disconnect")
+      if (_bus.IsConnected)
       {
-        Task.Run(
-          () =>
-            {
-              using (var bus = new Bus(connectorParameter))
-              {
-                try
-                {
-                  bus.Connect();
-                }
-                catch (Exception exception)
-                {
-                  Device.BeginInvokeOnMainThread(
-                    () =>
-                      {
-                        ConnectButtonText = "Connect";
-                        ConnectButtonColor = Color.GreenYellow;
-                        CrossToastPopUp.Current.ShowToastMessage(
-                          "Could not connect to " + _settings.InterfaceName + "(" + _settings.IP + ")");
-                      });
-
-                  return;
-                }
-
-                if (bus.IsConnected)
-                {
-                  Device.BeginInvokeOnMainThread(
-                    () =>
-                      {
-                        CrossToastPopUp.Current.ShowToastMessage(
-                          "Connected to " + _settings.InterfaceName + "(" + _settings.IP + ")");
-                      });
-
-                  var senderAddress = bus.LocalIndividualAddress;
-                  bus.GroupValueReceived += args =>
-                    {
-                      Device.BeginInvokeOnMainThread(() =>
-                        {
-                          Telegramms.Add(new Telegramm(args, DateTime.Now));
-                          OnPropertyChanged(nameof(Telegramms));
-                        });
-                    };
-
-                  ////TODO pretty hacky to cancel by text
-                  while (ConnectButtonText == "Disconnect")
-                  {
-                  }
-                }
-              }
-            });
-        ConnectButtonText = "Disconnect";
-        ConnectButtonColor = Color.Red;
+        Task.Run(() => Action(connectorParameter));
       }
       else
       {
+        _bus.Disconnect();
         Telegramms.Clear();
-        ConnectButtonText = "Connect";
-        ConnectButtonColor = Color.GreenYellow;
+        OnPropertyChanged(nameof(ConnectButtonColor));
+        OnPropertyChanged(nameof(ConnectButtonText));
+      }
+    }
+
+    private void Action(ConnectorParameters connectorParameter)
+    {
+      _bus = new Bus(connectorParameter);
+
+      try
+      {
+        _bus.Connect();
+        OnPropertyChanged(nameof(ConnectButtonColor));
+        OnPropertyChanged(nameof(ConnectButtonText));
+      }
+      catch (Exception exception)
+      {
+        Device.BeginInvokeOnMainThread(
+          () =>
+            {
+              CrossToastPopUp.Current.ShowToastMessage(
+                "Could not connect to " + _settings.InterfaceName + "(" + _settings.IP + ")");
+            });
+
+        return;
+      }
+
+      if (_bus.IsConnected)
+      {
+        Device.BeginInvokeOnMainThread(
+          () =>
+            {
+              CrossToastPopUp.Current.ShowToastMessage(
+                "Connected to " + _settings.InterfaceName + "(" + _settings.IP + ")");
+            });
+
+        var senderAddress = _bus.LocalIndividualAddress;
+        _bus.GroupValueReceived += args =>
+          {
+            Device.BeginInvokeOnMainThread(() =>
+              {
+                Telegramms.Add(new Telegramm(args, DateTime.Now));
+                OnPropertyChanged(nameof(Telegramms));
+              });
+          };
+
+        while (_bus.IsConnected)
+        {
+        }
       }
     }
 
