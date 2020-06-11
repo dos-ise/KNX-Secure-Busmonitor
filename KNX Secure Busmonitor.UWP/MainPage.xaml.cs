feac21c99b20b69ed897766b23af3d4d0a7738fd
Dennis Ostermann <dennis.ostermann@ise.de>
2020-06-11 09:47:22 +0200

    Added UWP Project

---
+++ KNX Secure Busmonitor.UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace KNX_Secure_Busmonitor.UWP
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage   
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+      LoadApplication(new Busmonitor.App());
+    }
+  }
+}
