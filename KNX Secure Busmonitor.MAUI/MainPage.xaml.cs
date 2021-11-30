2369a98f4b653e477d9afb6b66a1087c2a4932ef
Dennis Ostermann <dennis.ostermann@ise.de>
2021-11-30 12:53:11 +0100

    Added KnxBus.DiscoverIpDevicesAsync()

--- KNX Secure Busmonitor.MAUI/MainPage.xaml.cs
+++ KNX Secure Busmonitor.MAUI/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Maui.Controls;
+﻿using Knx.Falcon.Configuration;
+using Knx.Falcon.KnxnetIp;
+using Knx.Falcon.Sdk;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 using System;
 
@@ -13,10 +16,26 @@ namespace KNX_Secure_Busmonitor.MAUI
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
             CounterLabel.Text = $"Current count: {count}";
+
+            await foreach (var ip in KnxBus.DiscoverIpDevicesAsync())
+            {
+                Console.WriteLine(ip);
+
+                // convert to a connection string
+                var connectionString = ip.ToConnectionString();
+                Console.WriteLine(connectionString);
+
+                // or directly to a IPTunnelingConnectorParameters object
+                if (ip.Supports(ServiceFamily.Tunneling, 1))
+                {
+                    var connectorParameter = IpTunnelingConnectorParameters.FromDiscovery(ip);
+                }
+            }
+
 
             SemanticScreenReader.Announce(CounterLabel.Text);
         }
