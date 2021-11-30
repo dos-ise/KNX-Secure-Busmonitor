e73558da51f44c3d2bf4ad09d08265ec7bc9b973
Dennis Ostermann <dennis.ostermann@ise.de>
2021-11-30 10:13:18 +0100

    Updated MAUI Template

---
+++ KNX Secure Busmonitor.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace KNX_Secure_Busmonitor.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
