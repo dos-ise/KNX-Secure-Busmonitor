e02fc20d502ad44eab06de12be10507a4b89c6d2
Dennis Ostermann <dennis.ostermann@ise.de>
2021-12-01 11:39:19 +0100

    Added more test code to MAUI

--- KNX Secure Busmonitor.MAUI/MainPage.xaml.cs
+++ KNX Secure Busmonitor.MAUI/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Knx.Falcon.Configuration;
+﻿using Knx.Falcon;
+using Knx.Falcon.Configuration;
 using Knx.Falcon.KnxnetIp;
 using Knx.Falcon.Sdk;
 using Microsoft.Maui.Controls;
@@ -21,23 +22,48 @@ namespace KNX_Secure_Busmonitor.MAUI
             count++;
             CounterLabel.Text = $"Current count: {count}";
 
-            await foreach (var ip in KnxBus.DiscoverIpDevicesAsync())
+            var parameter = new IpTunnelingConnectorParameters("192.168.178.46");
+            try
             {
-                Console.WriteLine(ip);
+                var connection = new KnxBus(parameter);
+                connection.ConnectionStateChanged += _connection_OnStateChange;
+                connection.GroupMessageReceived += _connection_OnData;
+                connection.Connect();
+            }
+            catch (Exception ex)
+            {
 
-                // convert to a connection string
-                var connectionString = ip.ToConnectionString();
-                Console.WriteLine(connectionString);
-
-                // or directly to a IPTunnelingConnectorParameters object
-                if (ip.Supports(ServiceFamily.Tunneling, 1))
-                {
-                    var connectorParameter = IpTunnelingConnectorParameters.FromDiscovery(ip);
-                }
             }
 
 
+            //TODO discovery does not seem to work
+            //await foreach (var ip in KnxBus.DiscoverIpDevicesAsync())
+            //{
+            //    Console.WriteLine(ip);
+
+            //    // convert to a connection string
+            //    var connectionString = ip.ToConnectionString();
+            //    Console.WriteLine(connectionString);
+
+            //    // or directly to a IPTunnelingConnectorParameters object
+            //    if (ip.Supports(ServiceFamily.Tunneling, 1))
+            //    {
+            //        var connectorParameter = IpTunnelingConnectorParameters.FromDiscovery(ip);
+            //    }
+            //}
+
+
             SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+
+        private void _connection_OnData(object sender, GroupEventArgs e)
+        {
+
+        }
+
+        private void _connection_OnStateChange(object sender, EventArgs e)
+        {
+
         }
     }
 }
