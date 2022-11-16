7d2db8dc461230d2d6ca8c2440b9c033ec68f069
Dennis Ostermann <dennis.ostermann@ise.de>
2022-11-16 13:09:34 +0100

    Added Xamarin Forms Project for Tizen TV

---
+++ KNX Secure Busmonitor/KNX Secure Busmonitor.Tizen.TV/Services/TizenNotificationManager.cs
@@ -0,0 +1,27 @@
+﻿using Busmonitor.Bootstrap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KNX_Secure_Busmonitor.Tizen.TV.Services
+{
+    public class TizenNotificationManager : INotificationManager
+    {
+        public event EventHandler NotificationReceived;
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendNotification(string title, string message, DateTime? notifyTime = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceiveNotification(string title, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
