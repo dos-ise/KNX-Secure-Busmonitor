5ff2f4c0555b04a7cbeb4ebe0ecaf6429a0ecca5
Dennis Ostermann <dennis.ostermann@ise.de>
2023-06-23 11:47:42 +0200

    Added telegram class to maui

---
+++ KNX Secure Busmonitor MAUI/Model/Telegram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNX_Secure_Busmonitor_MAUI.Model
+{
+    internal class Telegram
+    {
+        public string Name { get; set; }
+        public string DestinationAddress { get; set; }
+        public string SourceAddress { get; set; }
+        public string Value { get; set; }
+    }
+}
--- KNX Secure Busmonitor MAUI/ViewModel/MainViewModel.cs
+++ KNX Secure Busmonitor MAUI/ViewModel/MainViewModel.cs
@@ -1,43 +1,80 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Knx.Falcon;
+using Knx.Falcon.Configuration;
 using Knx.Falcon.Sdk;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using KNX_Secure_Busmonitor_MAUI.Model;
 
 namespace KNX_Secure_Busmonitor_MAUI.ViewModel
 {
     [INotifyPropertyChanged]
     internal partial class MainViewModel
     {
+        private readonly KnxBus _bus;
+
         public MainViewModel()
         {
-            Task.Run(() =>
-            {
-                isRefreshing = true;
-                foreach (var dis in KnxBus.DiscoverIpDevices())
-                {
-                    telegrams.Add(dis.FriendlyName);
-                }
-                isRefreshing = false;
-            });
+            _bus = new KnxBus(CreateParameter());
+            Telegrams = new ObservableCollection<Telegram>();
+            //Telegrams.Add(new Telegram() { Name = "Test", DestinationAddress = "1/1/1", Value = "true" });
+            //Telegrams.Add(new Telegram() { Name = "Test", DestinationAddress = "1/1/1", Value = "true" });
+            //Telegrams.Add(new Telegram() { Name = "Test", DestinationAddress = "1/1/1", Value = "true" });
+            //Telegrams.Add(new Telegram() { Name = "Test", DestinationAddress = "1/1/1", Value = "true" });
+            //Telegrams.Add(new Telegram() { Name = "Test", DestinationAddress = "1/1/1", Value = "true" });
+            //Telegrams.Add(new Telegram() { Name = "Test", DestinationAddress = "1/1/1", Value = "true" });
+        }
+
+        private ConnectorParameters CreateParameter()
+        {
+            ////TODO Test
+            var ip = KnxBus.DiscoverIpDevices().ToList().FirstOrDefault()?.LocalIPAddress;
+            return new IpTunnelingConnectorParameters(ip?.MapToIPv4().ToString());
         }
 
         [ObservableProperty]
-        private List<string> telegrams;        
-        
+        private ObservableCollection<Telegram> telegrams;
+
         [ObservableProperty]
-        private string selectedTelegram;        
-        
+        private Telegram selectedTelegram;
+
         [ObservableProperty]
         private bool isRefreshing;
+
 
         [RelayCommand]
         private void Refresh()
         {
             Console.WriteLine("Hello!");
+        }
+
+        [RelayCommand]
+        private async void OnConnect()
+        {
+            try
+            {
+                if (_bus.ConnectionState != BusConnectionState.Connected)
+                {
+                    await _bus.ConnectAsync();
+                }
+
+                _bus.GroupMessageReceived += _bus_GroupMessageReceived;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void _bus_GroupMessageReceived(object sender, GroupEventArgs e)
+        {
+            Telegrams.Add(new Telegram() 
+            { 
+                Name = "TODO", 
+                DestinationAddress = e.DestinationAddress, 
+                SourceAddress = e.SourceAddress, 
+                Value = e.Value.ToString()
+            });
         }
     }
 }
